6ee2b0d07c1f34e9ebd2f44306e6ac75bb9b8d0b
heweiron <heweiron@hawaii.edu>
2022-01-14 22:17:22 -1000

    Initial Commit

---
+++ Figma/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Figma
+//
+//  Created by weirong he on 1/14/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Figma/FigmaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FigmaApp.swift
+//  Figma
+//
+//  Created by weirong he on 1/14/22.
+//
+
+import SwiftUI
+
+@main
+struct FigmaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
